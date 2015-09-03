960b9fcb855591ded7ac3f288a5959c4853c5551
MitchRivet <mitch.rivet@gmail.com>
2015-09-02 22:31:31 -0700

    add create store feature to index page

--- app/app.php
+++ app/app.php
@@ -4,4 +4,31 @@
     require_once __DIR__."/../src/Brand.php";
     require_once __DIR__."/../src/Store.php";
 
-    
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
+    $app = new Silex\Application();
+
+    $server = 'mysql:host=localhost:8889;dbname=shoes';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path'=> __DIR__.'/../views'
+    ));
+
+    $app->get("/", function() use ($app){
+        return $app['twig']->render('index.html.twig', array('stores' => Store::getAll()));
+    });
+
+    $app->post("/stores", function () use ($app) {
+        $store_name = $_POST['name'];
+        $new_store = new Store($store_name);
+        $new_store->save();
+        return $app['twig']->render('index.html.twig', array('stores' => Store::getAll()));
+    }); 
+
+    return $app;
+
+?>
